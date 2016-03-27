adc840ad8b3b778f54134e1a9ade13857c4daaa2
Zachary Uzupis <zuzupis@gmail.com>
2016-03-27 17:18:12 -0400

    added factories for post and user in factories.rb close #9

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,21 +3,28 @@ FactoryGirl.define do
     title "MyString"
     message "MyText"
   end
+  factory :user do
+    fname "Tay"
+    lname "Zonday"
+    username "PurpleRain"
+    email "tzonday@gmail.com"
+    password "password"
+  end
    #first factory is named after the class
-   factory :candy_store do
-      name "Skittlopolis"
-      specialty "Skittles"
-      phone_number "1-800-SKITTLE"
-      address "4102 Lower Rd., Newportville, PA 19056"
-      employees 5
-   end
+   # factory :candy_store do
+   #    name "Skittlopolis"
+   #    specialty "Skittles"
+   #    phone_number "1-800-SKITTLE"
+   #    address "4102 Lower Rd., Newportville, PA 19056"
+   #    employees 5
+   # end
    #this factory is named something other than the class, so it must
    #be given a class definition
-   factory :broken_candy_store, class: 'CandyStore' do
-      name ""
-      specialty "Skittles"
-      phone_number "1-800-SKITTLE"
-      address "4102 Lower Rd., Newportville, PA 19056"
-      employees 5
-   end
+   # factory :broken_candy_store, class: 'CandyStore' do
+   #    name ""
+   #    specialty "Skittles"
+   #    phone_number "1-800-SKITTLE"
+   #    address "4102 Lower Rd., Newportville, PA 19056"
+   #    employees 5
+   # end
 end
